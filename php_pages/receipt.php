02354388d43e66c71a63418d3508bd713271a0e7
Timothy Allen Johnson II <timothyallenjohnsonii@gmail.com>
2024-04-04 23:25:06 -0400

    Created receipt.php receipt.css

--- php_pages/receipt.php
+++ php_pages/receipt.php
@@ -4,9 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Receipt</title>
-
+    <link rel="stylesheet" href="../styles/receipt.css">
 </head>
+
 <body>
+    <header>
+        <h1>Welcome to Burger Joint</h1>
+    </header>
+
+    <!-- Navigation Bar -->
+    <nav class="navbar">
+        <div class="container">
+            <h1 class="logo">Burger Restaurant</h1>
+            <ul class="nav-links">
+                <li><a href="../php_pages/index.php">Home</a></li>
+                <li><a href="../php_pages/Menu.php">Menu</a></li>
+                <li><a href="../php_pages/about.php">About</a></li>
+                <li><a href="../php_pages/contact.php">Contact</a></li>
+                <li><a href="../php_pages/create_account.php">Create Account</a></li>
+            </ul>
+        </div>
+    </nav>
+
+
     <div class="receipt">
         <h1>Receipt</h1>
         <?php
@@ -20,4 +40,5 @@
         ?>
     </div>
 </body>
+
 </html>
